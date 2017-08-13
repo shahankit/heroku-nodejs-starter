67d8249949c9e104c372ea11bb921f6f4cc1a6db
Ankit Shah <shahankit2313@gmail.com>
2017-08-13 22:14:02 +0530

    Added required middlewares

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import 'babel-polyfill';
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import compression from 'compression';
 
 dotenv.config();
 
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+// Compression
+app.use(compression());
+
+// CORS Middleware
+app.use(cors());
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
